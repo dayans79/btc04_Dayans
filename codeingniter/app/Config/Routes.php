5ae7dad54339858eeb25aa1e54ac9e10f6bd8ceb
Dayans <84407238+dayans79@users.noreply.github.com>
2021-05-18 13:34:25 -0500

    Update Routes.php

--- codeingniter/app/Config/Routes.php
+++ codeingniter/app/Config/Routes.php
@@ -51,3 +51,5 @@ if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
 {
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
+$routes->get('/', 'LoginController::index');
+$routes->post('login', 'LoginController::login');
